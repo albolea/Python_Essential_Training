6a8fbc78bc33c133c1988ad7b3fbbfafc79c2824
Renato Albolea <albolea@gmail.com>
2019-10-23 17:21:17 -0700

    finished url_utilities.py

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,10 +1,8 @@
 def create_database(database_path: str):
-    #TODO: Generate the database
+    # TODO: Generate the database
     pass
 
 
 def save_words_to_database(database_path: str, word_list: list):
-    #TODO: save the words to the data base
+    # TODO: save the words to the data base
     pass
-
-
